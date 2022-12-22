4cc38915da6e53e9c7a7e2577f53dafb9de3b98c
hellobwjung <wannacry@gmail.com>
2022-12-22 10:01:55 +0900

    Create day07_virus_user.cpp

---
+++ expert_day07/day07_virus_user.cpp
@@ -0,0 +1,16 @@
+/*
+ * day07_virus_user.cpp
+ *
+ *  Created on: 2022. 12. 13.
+ *      Author: bw.jung
+ */
+
+/// *****  user.cpp *****
+static const int USER_MAX = 50'000'000;
+
+extern char virus_checkup(int n);
+extern void culture(int n, int a, int b);
+extern void clear(int n);
+
+void test() {
+}
