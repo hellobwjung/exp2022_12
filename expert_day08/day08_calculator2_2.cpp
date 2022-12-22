9b30556a69a74e562d44eea4bce3133304f39b1f
hellobwjung <wannacry@gmail.com>
2022-12-22 10:08:10 +0900

    Create day08_calculator2_2.cpp

---
+++ expert_day08/day08_calculator2_2.cpp
@@ -0,0 +1,16 @@
+/*
+ * day08_calculator2_2.cpp
+ *
+ *  Created on: 2022. 12. 14.
+ *      Author: bw.jung
+ */
+
+#include <cstdio>
+
+const int LM = 222;
+char exp[LM];
+
+int main() {
+
+    return 0;
+}
